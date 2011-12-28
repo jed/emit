654bbf14da2796cecd8295f0e02f00fd6a439bb2
Jed Schmidt <tr@nslator.jp>
2011-12-27 20:44:20 -0500

    add RegExp

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ exports.ops       =
 exports.operators = require("./lib/operators")
 exports.Function  = require("./lib/Function")
 exports.Math      = require("./lib/Math")
+exports.RegExp    = require("./lib/RegExp")
